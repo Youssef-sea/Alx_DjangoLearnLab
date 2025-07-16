8d8cbdc03a0a65c5583a73590aabc56dbd0e5900
Youssef-sea <youssef-laftouhi@hotmail.fr>
2025-07-17 00:59:47 +0100

    week_10

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,10 @@
+# relationship_app/urls.py
+from django.urls import path
+from . import views
+
+app_name = 'relationship_app' # Namespace for the app's URLs
+
+urlpatterns = [
+    path('books/', views.book_list, name='book_list'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+# relationship_app/views.py
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Library # Import Library for the class-based view
 
-# Create your views here.
+# Function-based view to list all books
+def book_list(request):
+    books = Book.objects.all().order_by('title')
+    context = {
+        'books': books
+    }
+    return render(request, 'relationship_app/list_books.html', context)
+
+# Class-based view to display details for a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library' # The variable name to use in the template
+
+    # Optional: If you need to add extra context or do more complex things
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     # You can add more context here if needed
+    #     return context
