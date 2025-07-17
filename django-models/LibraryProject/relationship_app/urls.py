ef230abf37f97ce0791a49395b841055461e0baf
Youssef-sea <youssef-laftouhi@hotmail.fr>
2025-07-17 01:58:28 +0100

    week_10

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,8 +7,12 @@ app_name = 'relationship_app' # Namespace for the app's URLs
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
- # Authentication views
-    path('register/', register, name='register'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
+ # Authentication views  
+    path('register/', register, name='register'), # Already uses views.register
+    path('login/',
+         LoginView.as_view(template_name='relationship_app/login.html'), # <--- LoginView directly
+         name='login'),
+    path('logout/',
+         LogoutView.as_view(next_page=reverse_lazy('relationship_app:login'), template_name='relationship_app/logout.html'), # <--- LogoutView directly
+         name='logout'),
 ]
