5c719b8633442fce64272f2566fd0cfb86f00703
Youssef-sea <youssef-laftouhi@hotmail.fr>
2025-07-17 23:59:06 +0100

    week_10

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -23,8 +23,8 @@ urlpatterns = [
     path('member_dashboard/', member_view, name='member_view'),  
  
 # NEW: URLs for custom permission-based book actions
-    path('books/add/', add_book, name='add_book'),
-    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
-    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:pk>/edit/', edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/delete/', delete_book, name='delete_book'),
 
 ]
