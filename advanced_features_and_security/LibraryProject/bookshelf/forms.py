c3c00eb9928f9e115c558167f2804156cd9a6306
Youssef-sea <youssef-laftouhi@hotmail.fr>
2025-07-26 17:12:15 +0100

    week_11

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -6,3 +6,11 @@ class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ['title', 'content']
+
+# New: ExampleForm for general purpose input
+
+class ExampleForm(forms.Form):
+    your_name = forms.CharField(label="Your Name", max_length=100)
+    your_email = forms.EmailField(label="Your Email")
+    message = forms.CharField(label="Your Message", widget=forms.Textarea)
+    newsletter_signup = forms.BooleanField(label="Sign up for newsletter?", required=False)
