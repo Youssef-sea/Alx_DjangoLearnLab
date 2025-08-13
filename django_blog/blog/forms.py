b9eafdd8ce722f6cc3ce13c9bbc76db159f0e19c
Youssef-sea <youssef-laftouhi@hotmail.fr>
2025-08-13 19:27:15 +0100

    week_14

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,10 +2,18 @@
 from django import forms
 from .models import Post, Comment
 
+class TagWidget(forms.TextInput):
+    def __init__(self, *args, **kwargs):
+        attrs = {'class': 'tag-input'}
+        super().__init__(attrs=attrs, *args, **kwargs)
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
     pass
 
 class CommentForm(forms.ModelForm):
