214dfe3fe8584dd82a1af8613ec5db368d17ab1f
Youssef-sea <youssef-laftouhi@hotmail.fr>
2025-08-20 00:07:28 +0100

    week 15

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
-    path('follow/<int:pk>/', FollowUnfollowView.as_view(), name='follow-unfollow'),
+    path('follow/<int:user_id>/', FollowUnfollowView.as_view(), name='follow-unfollow'),
 ]
