8150b973537108ba830638e97644ecd201748d3b
Youssef-sea <youssef-laftouhi@hotmail.fr>
2025-08-20 00:04:52 +0100

    week 15

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,7 +2,6 @@
 
 from rest_framework import viewsets, permissions, generics
 from rest_framework.filters import SearchFilter
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from .models import Post, Comment
@@ -13,12 +12,8 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     Custom permission to only allow authors of an object to edit it.
     """
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        # Write permissions are only allowed to the author of the post.
         return obj.author == request.user
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -37,8 +32,6 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
-        # We assume the post ID is passed in the URL, e.g., /posts/1/comments/
-        # This is a common practice when using nested routers.
         post_id = self.kwargs['post_pk']
         post = Post.objects.get(pk=post_id)
         serializer.save(author=self.request.user, post=post)
@@ -49,7 +42,8 @@ class FeedView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        # Get the users that the current user is following
-        followed_users = self.request.user.following.all()
-        # Return posts from those users, ordered by creation date
-        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        # Retrieve the users the current user is following.
+        following_users = self.request.user.following.all()
+        
+        # Filter posts by those authors and order them by creation date.
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
