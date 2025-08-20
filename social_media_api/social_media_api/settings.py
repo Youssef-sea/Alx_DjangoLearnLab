fe324c87a88697ae18b136f933c74c078d74d44c
Youssef-sea <youssef-laftouhi@hotmail.fr>
2025-08-20 15:42:17 +0100

    week_15

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -98,6 +98,7 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
 }
+ ["PORT"]
 
 
 # Password validation
